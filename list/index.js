f763c875aedc297bc533c7b666a677a1f0224218
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-08-22 17:54:27 +0000

    feat: added the RelatedList class
    The RelatedList class has been created
    Added class tests

---
+++ list/index.js
@@ -0,0 +1,40 @@
+const Item = require("../item");
+
+class RelatedList {
+  constructor() {
+    this._head = null;
+    this._tail = null;
+    this._current = null;
+  }
+
+  start() {
+    this._current = this._head;
+    return this.current();
+  }
+
+  next() {
+    if (this._current) {
+      this._current = this._current.getNext;
+      return this.current();
+    } else {
+      return this.start();
+    }
+  }
+
+  current() {
+    if (!this._current) return null;
+    return this._current.content;
+  }
+
+  add(value) {
+    const item = new Item(value);
+    if (this._tail) {
+      this._tail.next = item;
+    } else {
+      this._head = item;
+    }
+    this._tail = item;
+  }
+}
+
+module.exports = RelatedList;
