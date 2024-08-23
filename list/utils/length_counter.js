9a748437695aef1a37f4b4a72fe3ade750a113c0
Григорий <122614065+Grigorasch@users.noreply.github.com>
2024-08-23 05:20:37 +0000

    docs: added documentation for the length counter

--- list/utils/length_counter.js
+++ list/utils/length_counter.js
@@ -1,3 +1,16 @@
+/**
+ * Интерфейс счётчика предоставляющий доступ к значению счётчика, методам увеличения и уменьшения. Если в текущем списке выключен параметр lengthCount, то вычисление длины производится не будет и счетчик всегда будет возвращать false.
+ * @typedef {Object} Counter
+ * @property {function(): number|boolean} counter - Возвращает текущее значение.
+ * @property {function(): void} add - Метод для увеличения счетчика.
+ * @property {function(): void} remove - Метод для уменьшения счетчика.
+ */
+
+/**
+ * Функция, которая возвращает настоящий счётчик длины.
+ * @returns {Counter} Настоящий счётчик
+ * @since 0.2.0
+ */
 module.exports.accept = function accept() {
   let counter = 0;
   const length = function () {
@@ -13,6 +26,11 @@ module.exports.accept = function accept() {
   return length;
 };
 
+/**
+ * Функция, которая возвращает фейковый счётчик длины. Фейковый счётчик всегда возвращает false.
+ * @returns {Counter} Фейковый счётчик
+ * @since 0.2.0
+ */
 module.exports.reject = function reject() {
   const fakeLength = function () {
     return false;
