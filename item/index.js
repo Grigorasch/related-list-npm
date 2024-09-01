bce4989eff32ee8e56924cb38a40e07b2af43aae
Grigorasch <pgv856@gmail.com>
2024-09-01 09:33:00 +0600

    docs: documentation has been updated

--- item/index.js
+++ item/index.js
@@ -24,20 +24,21 @@ class Item {
   /**
    * Устанавливает следующий элемент в списке.
    *
-   * @param {Item|null} next - Следующий элемент. Если передано значение undefinded, то содержимое элемента будет удалено.
+   * @param {Item|null} next - Следующий элемент. Если передано значение undefined, то содержимое элемента будет удалено.
    * @throws {TypeError} Если next не является экземпляром Item.
    * @since 0.1.0
+   * @version 0.4.0
    */
-  set next(next = null) {
+  set next(next) {
     if (next && !(next instanceof Item))
       throw new TypeError("Next item must be an instance of Item");
-    this._next = next;
+    this._next = next ?? null;
   }
 
   /**
    * Возвращает следующий элемент в списке.
    *
-   * @returns {*} Следующий элемент или null.
+   * @returns {any|null} Следующий элемент или null.
    * @since 0.1.0
    */
   get next() {
@@ -47,21 +48,23 @@ class Item {
   /**
    * Устанавливает предыдущий элемент в списке.
    *
-   * @param {Item|null} previous - Предыдущий элемент. Если передано значение undefinded, то содержимое элемента будет удалено.
+   * @param {Item|null} previous - Предыдущий элемент. Если передано значение undefined, то содержимое элемента будет удалено.
    * @throws {TypeError} Если previous не является экземпляром Item.
    * @since 0.2.0
+   * @version 0.4.0
    */
-  set previous(previous = null) {
+  set previous(previous) {
     if (previous && !(previous instanceof Item))
       throw new TypeError("Previous item must be an instance of Item");
-    this._previous = previous;
+    this._previous = previous ?? null;
   }
 
   /**
    * Возвращает предыдущий элемент в списке.
    *
-   * @returns {*} Предыдущий элемент. Для первого элемента в списке или при отсутсвии предыдущего элемента возвращае null.
+   * @returns {any|null} Предыдущий элемент. Для первого элемента в списке или при отсутствии предыдущего элемента возвращаем null.
    * @since 0.2.0
+   * @version 0.4.0
    */
   get previous() {
     return this._previous;
@@ -70,17 +73,18 @@ class Item {
   /**
    * Устанавливает содержимое элемента.
    *
-   * @param {*} content - Новое содержимое элемента. Если передано значение undefinded, то содержимое элемента будет удалено.
+   * @param {any} content - Новое содержимое элемента. Если передано значение undefined, то содержимое элемента будет удалено.
    * @since 0.1.0
+   * @version 0.4.0
    */
-  set content(content = null) {
-    this._content = content;
+  set content(content) {
+    this._content = content ?? null;
   }
 
   /**
    * Возвращает содержимое элемента.
    *
-   * @returns {*} Содержимое элемента. Пустой элемент возвращает null.
+   * @returns {any|null} Содержимое элемента. Пустой элемент возвращает null.
    * @since 0.1.0
    */
   get content() {
